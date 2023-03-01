9aaf90a6670b90fd78db48f740abaddd66d77fce
giomikadze <110746808+giomikadze@users.noreply.github.com>
2023-03-01 18:18:49 +0100

    js-1

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+let num = prompt("Введите число");
+console.log(num ** 2);
+
+
+let number  = prompt('выберите число')
+if (+number  ===0) {
+  console.log('число ровен чулю') 
+}
+if (+number % 2===0  && + number!==0){
+  console.log('четное число')
+}
+ if(+number % 2===1){
+  console.log("нечетное число")
+ }
+
+
+
+
+
+// let variable = prompt('введите значение')
+//     if( variable == +variable){
+//     console.log('значение число')
+//     } else{ 
+//         console.log(isNaN())
+//     }
+
+
+
+
